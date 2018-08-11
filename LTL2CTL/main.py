792b032f8d2719a0232f1a9ef78b97e96b35a1a7
idanb55 <idanb55@gmail.com>
2018-08-11 17:49:31 +0300

    rearranged some stuff and converted the el list to dictionary

--- LTL2CTL/main.py
+++ LTL2CTL/main.py
@@ -4,7 +4,24 @@ from elementary import get_elementary_formulas
 # get the model from file
 # 
 
-print get_elementary_formulas('[[a]U[b]]U[~[X[a]]]')
+formula = '[a]U[b]'
+model = {
+	1: set([2,3,4]),
+	2: set([1]),
+	3: set([4]),
+	4: set([4]),
+}
+
+def convert_list_to_index_dictionary(l):
+	d = {}
+	for i in xrange(len(l)):
+		d[l[i]] = i
+	return d
+
+el = get_elementary_formulas(formula)
+el_dict = convert_list_to_index_dictionary(el)
+print el_dict
+print model
 
 if __name__ == '__main__':
 	print "Hello, World!"
