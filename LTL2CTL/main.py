134e4bd3f2990a173e99b7a2096225d121f07a5d
idanb55 <idanb55@gmail.com>
2018-08-11 17:40:42 +0300

    minor

--- LTL2CTL/main.py
+++ LTL2CTL/main.py
@@ -1,10 +1,10 @@
-import elementary
+from elementary import get_elementary_formulas
 
 # parse LTL formula
 # get the model from file
 # 
 
-print elementary.get_elementary_formulas('[[a]U[b]]U[~[X[a]]]')
+print get_elementary_formulas('[[a]U[b]]U[~[X[a]]]')
 
 if __name__ == '__main__':
 	print "Hello, World!"
