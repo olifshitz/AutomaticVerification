f6b037384b46147e665c1d6c1418f4aeb2dd0cb7
idanb55 <idanb55@gmail.com>
2018-08-14 23:04:21 +0300

    we now have the product model

--- LTL2CTL/main.py
+++ LTL2CTL/main.py
@@ -10,15 +10,21 @@ el = get_elementary_formulas(formula)
 el_bdds = convert_list_to_index_dictionary(el)
 
 a,b = map(bddvar,'ab')
+a_other,b_other=bddvar('a^'),bddvar('b^')
 
 #model
-msb = bddvars('$', 2) # model states bits: 1=00, 2=01, 3=10, 4=11
-msb_other = bddvars('%', 2) # model states bits: 1=00, 2=01, 3=10, 4=11
+model_index_length = 2
+
+msb = bddvars('$', model_index_length) # model states bits: 1=00, 2=01, 3=10, 4=11
+msb_other = bddvars('%', model_index_length) # model states bits: 1=00, 2=01, 3=10, 4=11
 
 model_atomic = (~msb[0] & ~msb[1]) & a & ~b # 1->a
 model_atomic |= (msb[0] & ~msb[1]) & a & b # 2->ab
 model_atomic |= (~msb[0] & msb[1]) & ~a & ~b # 3->[]
 model_atomic |= (msb[0] & msb[1]) & ~a & b # 4->b
+
+model_atomic_other = model_atomic.compose({msb[i]:msb_other[i] for i in range(model_index_length)})
+model_atomic_other = model_atomic_other.compose({a:a_other,b:b_other})
 
 model_relations = (~msb[0] & ~msb[1]) & (msb_other[0] | msb_other[1]) # 1->2, 1->3, 1->4
 model_relations |= (msb[0] & ~msb[1]) & (~msb_other[0] & ~msb_other[1]) # 2->1
@@ -29,12 +35,20 @@ print("model atomic", list(model_atomic.satisfy_all()))
 print("model relations", list(model_relations.satisfy_all()))
 
 print("elemntary bdds", el_bdds)
-print("sat(f)", list(get_sat(formula, el_bdds).satisfy_all()))
-print("tableau relations", list(get_relation_table(el_bdds).satisfy_all()))
+initial_states = get_sat(formula, el_bdds)
+print("sat(f)", list(initial_states.satisfy_all()))
+tableau_relations = get_relation_table(el_bdds)
+print("tableau relations", list(tableau_relations.satisfy_all()))
 
 fairness_constraints = get_all_fairness_constraints(formula, el_bdds)
 print("product fairness length", len(fairness_constraints))
 print("product fairness [0]", list(fairness_constraints[0].satisfy_all()))
+
+product_relations = model_relations & tableau_relations & model_atomic & model_atomic_other
+for rel in product_relations.satisfy_all():
+	print("product relation", rel)
+
+print("product fairness [0]", list(product_relations.restrict({a:1,b:1}).satisfy_all()))
 
 print('Nodes: %d' % len(_NODES))
 
