5a41aca81adff7c4d28dc8d8a0428086e8c3cddc
idanb55 <idanb55@gmail.com>
2018-08-20 20:29:18 +0300

    moved elementary implementation to tableau

--- LTL2CTL/tableau.py
+++ LTL2CTL/tableau.py
@@ -1,11 +1,12 @@
-from elementary import *
-from sat import *
+from pyeda.inter import *
+from collections import deque
+from formula_parser import *
 from symbolic_model import SymbolicModel, Graph
 import bdd_utils
 
 class Tableau():
     def __init__(self, formula, atomic_str):
-        el = set(get_elementary_formulas(formula)) | set(atomic_str)
+        el = set(self.get_elementary_formulas(formula)) | set(atomic_str)
         self.el_bdds = convert_list_to_index_dictionary(el)
         self.el_bdds_other = convert_list_to_index_dictionary(el, consts.TAG_IDENTIFIER)
 
@@ -17,7 +18,7 @@ class Tableau():
         bdd_utils.print_debug_bdd('tableau sat(f)', self.initial_states)
         bdd_utils.print_debug_bdd('tableau rel', self.relations)
 
-        self.fairness_constraints = get_all_fairness_constraints(formula, self)
+        self.fairness_constraints = self.get_fairness_constraints(formula)
         for fair in self.fairness_constraints:
             bdd_utils.print_debug_bdd('fairness', fair)
 
@@ -46,6 +47,45 @@ class Tableau():
 
         return Graph(global_compose, product_relations)
 
+    def get_elementary_formulas(self, formula):
+        el = []
+        formulas_to_check = deque([formula])
+        while len(formulas_to_check):
+            cur_formula = formulas_to_check.popleft()
+            op, form_g, form_h = parse_next_step(cur_formula)
+            if not op:
+                if (cur_formula not in el):
+                    assert form_g.islower() or form_g == consts.DUMMY_ATOMIC_PROPOSITION
+                    el.append(form_g)
+                continue
+            if op == consts.NOT_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                continue
+            if op == consts.NEXT_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                el.append(cur_formula)
+                continue
+            if op == consts.GLOBALY_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                el.append(FormConst.f_next(FormConst.f_globally(form_g)))
+                continue
+            if op == consts.EVENTUALLY_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                el.append(FormConst.f_next(FormConst.f_eventually(form_g)))
+                continue
+            if op in (consts.OR_IDENTIFIER, consts.AND_IDENTIFIER):
+                formulas_to_check.append(form_g)
+                formulas_to_check.append(form_h)
+                continue
+            if op == consts.UNTIL_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                formulas_to_check.append(form_h)
+                el.append(get_next_until_form(form_g, form_h))
+                continue
+            raise Exception('Not my problem %s' % (formula,))
+        return el
+
+
 
     def get_sat(self, formula, use_other_el=False):
         if not use_other_el and formula in self.el_bdds:
@@ -66,4 +106,35 @@ class Tableau():
         if op == consts.UNTIL_IDENTIFIER:
             return self.get_sat(form_h, use_other_el) | (self.get_sat(form_g, use_other_el) &
                     self.get_sat(get_next_until_form(form_g, form_h), use_other_el))
-        raise Exception('Unsupported operator %s' % (formula,))
+        raise Exception('Unsupported operator by <get_sat>: %s' % (formula,))
+
+    def get_fairness_constraints(self, formula):
+        fairness = []
+        formulas_to_check = deque([formula])
+        while len(formulas_to_check):
+            cur_formula = formulas_to_check.popleft()
+            op, form_g, form_h = parse_next_step(cur_formula)
+            if not op:
+                continue
+            if op in (consts.NOT_IDENTIFIER, consts.NEXT_IDENTIFIER):
+                formulas_to_check.append(form_g)
+                continue
+            if op in (consts.OR_IDENTIFIER, consts.AND_IDENTIFIER):
+                formulas_to_check.append(form_g)
+                formulas_to_check.append(form_h)
+                continue
+            if op == consts.UNTIL_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                formulas_to_check.append(form_h)
+                fairness.append(self.get_sat(FormConst.f_or(FormConst.f_not(FormConst.f_until(form_g, form_h)), form_h)))
+                continue
+            if op == consts.GLOBALY_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                fairness.append(self.get_sat(FormConst.f_or(FormConst.f_globally(form_g), FormConst.f_not(form_g))))
+                continue
+            if op == consts.EVENTUALLY_IDENTIFIER:
+                formulas_to_check.append(form_g)
+                fairness.append(self.get_sat(FormConst.f_or(FormConst.f_not(FormConst.f_eventually(form_g)), form_g)))
+                continue
+            raise Exception('Unsupported operator by <get_fairness_constraints>: %s' % (formula,))
+        return fairness
